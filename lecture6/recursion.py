6995653ccb3b20af45de457a802fa5734de4d915
k7seven <kdoubleseven7@gmail.com>
2023-09-14 16:44:02 -0300

    more recursion problems

--- lecture6/recursion.py
+++ lecture6/recursion.py
@@ -12,4 +12,22 @@ def print_name(n, name):
     print(name)
     print_name(n - 1, name)
 
-print_name(2, "k77")
+# print_name(2, "k77")
+
+# problem 2 - print from 1 to n
+def print_1_to_n(i, n):
+    print(i)
+    if(i >= n):
+        return
+    print_1_to_n(i + 1, n)
+
+# print_1_to_n(1, 10)
+
+# problem 3 - print from n to 1
+def print_n_to_1(n):
+    if(n == 0):
+        return
+    print(n)
+    print_n_to_1(n - 1)
+
+# print_n_to_1(20)
