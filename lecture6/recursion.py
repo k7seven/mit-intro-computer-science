8234cf10875e4102cf04f07f0ba5ebf568acc219
k7seven <kdoubleseven7@gmail.com>
2023-09-15 11:33:36 -0300

    parametized and functional recursion

--- lecture6/recursion.py
+++ lecture6/recursion.py
@@ -31,3 +31,46 @@ def print_n_to_1(n):
     print_n_to_1(n - 1)
 
 # print_n_to_1(20)
+
+# problem 4 - print from 1 to n using backtracking
+def print_1_to_n_backtracking(n):
+    if(n < 1):
+        return
+    print_1_to_n_backtracking(n - 1)
+    print(n)
+
+# print_1_to_n_backtracking(10)
+
+# problem 5 - print from n to 1 using backtracking
+def print_n_to_1_backtracking(i, n):
+    if(i > n):
+        return
+    print_n_to_1_backtracking(i + 1, n)
+    print(i)
+
+# print_n_to_1_backtracking(1, 10)
+
+# problem 6 - print the sum of 1 to i (Parameterised way)
+def sum_1_to_n(i, sum):
+    if (i < 1):
+        print(sum)
+        return
+    sum_1_to_n(i - 1, sum + i)
+
+# sum_1_to_n(6, 0)
+
+# problem 7 - print the sum of 1 to i (Functional way)
+def sum_1_to_n_functional_way(n):
+    if (n == 0):
+        return 0
+    return n + sum_1_to_n_functional_way(n - 1)
+
+# print(sum_1_to_n_functional_way(5))
+
+# problem 8 - print the factorial of n
+def factorial(n):
+    if (n == 1):
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(4))
