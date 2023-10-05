d6d1bea12608ce269eecdc989689018913d4e570
k7seven <kdoubleseven7@gmail.com>
2023-10-05 18:34:54 -0300

    Problem Set 2 - Hangman

--- pset2/hangman.py
+++ pset2/hangman.py
@@ -51,8 +51,19 @@ wordlist = load_words()
 
 
 def is_word_guessed(secret_word, letters_guessed):
-
-    pass
+    '''
+    secret_word: string, the word the user is guessing; assumes all letters are
+      lowercase
+    letters_guessed: list (of letters), which letters have been guessed so far;
+      assumes that all letters are lowercase
+    returns: boolean, True if all the letters of secret_word are in letters_guessed;
+      False otherwise
+    '''
+    # FILL IN YOUR CODE HERE AND DELETE "pass"
+    for letter in secret_word:
+      if letter not in letters_guessed:
+        return False
+    return True
 
 
 
@@ -64,7 +75,13 @@ def get_guessed_word(secret_word, letters_guessed):
       which letters in secret_word have been guessed so far.
     '''
     # FILL IN YOUR CODE HERE AND DELETE "pass"
-    pass
+    guessed_words = []
+    for letter in secret_word:
+      if letter in letters_guessed:
+        guessed_words.append(letter)
+      else:
+        guessed_words.append("_ ")
+    return guessed_words
 
 
 
@@ -75,7 +92,39 @@ def get_available_letters(letters_guessed):
       yet been guessed.
     '''
     # FILL IN YOUR CODE HERE AND DELETE "pass"
-    pass
+    available_letters = string.ascii_lowercase
+    for letter in letters_guessed:
+      if letter in available_letters:
+        available_letters = available_letters.replace(letter, "")
+    return available_letters
+
+
+
+def welcome_message(word_length, guesses_left, available_letters):
+  print("Welcome to the game Hangman!")
+  print("I am thinking of a word that is", word_length, "letters long.")
+  print("-------------")
+  print("Available letters:", available_letters)
+
+
+
+def ask_user_input(available_letters):
+  letter_guessed = input("Please guess a letter: ")
+  if(letter_guessed.isalpha() and len(letter_guessed) == 1 and letter_guessed in available_letters):
+    return letter_guessed
+  elif(letter_guessed not in available_letters):
+    print("That letter was guessed before.")
+    return False
+  return False
+
+
+
+def is_letter_guessed_correct(letter_guessed, secret_word):
+  if(letter_guessed in secret_word):
+    return True
+  else:
+    return False
+
 
 
 
@@ -105,7 +154,72 @@ def hangman(secret_word):
     Follows the other limitations detailed in the problem write-up.
     '''
     # FILL IN YOUR CODE HERE AND DELETE "pass"
-    pass
+    word_length = len(secret_word)
+    guesses_left = 6
+    available_letters = get_available_letters([])
+    number_of_warnings = 3
+    letters_guessed = []
+    word_guessed = False
+
+    # greets the users
+    welcome_message(word_length, guesses_left, available_letters)
+
+    # if the user doesnt input correct single alphabet letter
+    # reduce the number of warnings
+    # if warnings = 0 he loses a guess
+    while(is_word_guessed(secret_word, letters_guessed) is False):
+      print("You have", guesses_left, "guesses left.")
+      letter_guessed = ask_user_input(available_letters)
+
+      # this while loop asks the users for a new input every time he insert an invalid letter
+      while(letter_guessed is False):
+        number_of_warnings -= 1
+        if(number_of_warnings > 0):
+          print("Oops! That is not a valid letter. You have", number_of_warnings, "warnings left.")
+          print("------------")
+        elif(guesses_left == 1 and number_of_warnings == 0):
+          print("You ran out of guesses. You lost!")
+          print("The word was", secret_word)
+          return
+        else:
+          guesses_left -= 1
+          number_of_warnings = 3
+          print("Oops! You have no more warnings. You have lost a guess. Your warnings have been reset to 3.")
+          print("------------")
+
+        letter_guessed = ask_user_input(available_letters)
+
+      # this append the letter guessed by the user to the letters_guessed list
+      letters_guessed.append(letter_guessed)
+
+      available_letters = get_available_letters(letters_guessed)
+
+      if(is_letter_guessed_correct(letter_guessed, secret_word)):
+        print("Good guess:", get_guessed_word(secret_word, letters_guessed))
+        print("------------")
+        print("Available letters:", available_letters)
+      elif(letter_guessed in "aeiou"):
+        guesses_left -= 2
+        print("Oops! That letter is not in my word:", get_guessed_word(secret_word, letters_guessed))
+        print("You lose 2 guessed because it was a vowel.")
+        print("------------")
+        if (guesses_left <= 0):
+          print("You ran out of guesses. You lost!")
+          print("The word was", secret_word)
+          return
+      else:
+        guesses_left -= 1
+        print("Oops! That letter is not in my word:", get_guessed_word(secret_word, letters_guessed))
+        print("------------")
+        if (guesses_left <= 0):
+          print("You ran out of guesses. You lost!")
+          print("The word was", secret_word)
+          return
+        print("Available letters:", available_letters)
+
+    print("Congratulations, you won!")
+
+
 
 
 
