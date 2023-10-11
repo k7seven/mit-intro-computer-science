bcaf1ac37e03c4168e767fc23f8c69fc5936ab9a
k7seven <kdoubleseven7@gmail.com>
2023-10-11 10:26:22 -0300

    classes and inheritance

--- lecture9/bank_accounts.py
+++ lecture9/bank_accounts.py
@@ -1,3 +1,6 @@
+class BalanceException(Exception):
+    pass
+
 class BankAccount:
     def __init__(self, initial_amount, account_name):
         self.balance = initial_amount
@@ -13,5 +16,50 @@ class BankAccount:
         print("\nDeposit complete.")
         self.get_balance()
 
+    def viable_transaction(self, amount):
+        if self.balance >= amount:
+            return
+        else:
+            raise BalanceException(
+                f"\nSorry, account {self.name} only has a balance of {self.balance:.2f}"
+            )
+
     def withdraw(self, amount):
-        self.balance -= amount
+        try:
+            self.viable_transaction(amount)
+            self.balance -= amount
+            print("\nWithdraw complete.")
+            self.get_balance()
+        except BalanceException as error:
+            print(f'\nWithdraw interrupted: {error}')
+
+    def transfer(self, amount, recepient):
+        try:
+            print('\n**********\n\nBeginning transfer... 🚀')
+            self.viable_transaction(amount)
+            self.withdraw(amount)
+            recepient.deposit(amount)
+            print('\nTransfer complete! ✅\n\n**********')
+        except BalanceException as error:
+            print(f'\nTransfer interrupted. ❌ {error}')
+
+
+class InterestRewardsAccount(BankAccount):
+    def deposit(self, amount):
+        self.balance = self.balance + (amount * 1.05)
+        print("\nDeposit complete.")
+        self.get_balance()
+
+class SavingsAccount(InterestRewardsAccount):
+    def __init__(self, initial_amount, account_name):
+        super().__init__(initial_amount, account_name)
+        self.fee = 5
+
+    def withdraw(self, amount):
+        try:
+            self.viable_transaction(amount + self.fee)
+            self.balance -= amount + self.fee
+            print("\nWithdraw completed.")
+            self.get_balance()
+        except BalanceException as error:
+            print(f'\Withdraw interrupted: {error}')
--- lecture9/main.py
+++ lecture9/main.py
@@ -3,4 +3,18 @@ from bank_accounts import *
 Dave = BankAccount(1000, "Dave")
 Sara = BankAccount(2000, "Sara")
 
-Dave.deposit(500)
+Dave.withdraw(15000)
+
+Dave.transfer(1000, Sara)
+
+Jim = InterestRewardsAccount(1000, "Jim")
+
+Jim.deposit(1000)
+
+Blaze = SavingsAccount(1000, "Blaze")
+
+Blaze.get_balance()
+
+Blaze.deposit(100)
+
+Blaze.transfer(1000, Sara)
