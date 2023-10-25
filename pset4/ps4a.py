10554b2bf1f1116a6d3b687ac66cb18925d3fe42
k7seven <kdoubleseven7@gmail.com>
2023-10-24 22:56:23 -0300

    ps4a done

--- pset4/ps4a.py
+++ pset4/ps4a.py
@@ -1,14 +1,12 @@
 # Problem Set 4A
-# Name: <your name here>
-# Collaborators:
-# Time Spent: x:xx
+# Name: k77
 
 def get_permutations(sequence):
     '''
     Enumerate all permutations of a given string
 
     sequence (string): an arbitrary string to permute. Assume that it is a
-    non-empty string.  
+    non-empty string.
 
     You MUST use recursion for this part. Non-recursive solutions will not be
     accepted.
@@ -22,8 +20,18 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
+    if len(sequence) == 1:
+        return [sequence]
 
-    pass #delete this line and replace with your code here
+    perms = get_permutations(sequence[1:])
+    char = sequence[0]
+    result = []
+
+    for perm in perms:
+        for i in range(len(perm) + 1):
+            result.append(perm[:i] + char + perm[i:])
+
+    return result
 
 if __name__ == '__main__':
 #    #EXAMPLE
@@ -31,10 +39,12 @@ if __name__ == '__main__':
 #    print('Input:', example_input)
 #    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
 #    print('Actual Output:', get_permutations(example_input))
-    
+
 #    # Put three example test cases here (for your sanity, limit your inputs
-#    to be three characters or fewer as you will have n! permutations for a 
+#    to be three characters or fewer as you will have n! permutations for a
 #    sequence of length n)
 
-    pass #delete this line and replace with your code here
-
+    example_input = 'abc'
+    print('Input:', example_input)
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations(example_input))
